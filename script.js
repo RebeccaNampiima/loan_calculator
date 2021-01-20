46db6ce42b88bb2a10293d060dc12d09ef5fe63d
rebecca <beckyann22@gmail.com>
2021-01-20 09:44:44 +0100

    first div

---
+++ script.js
@@ -0,0 +1,22 @@
+var slider = document.getElementById("myRange");
+var output = document.getElementById("value");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+  /*if(output= 0 && output>100000){
+      output.innerHTML = this.value.display = "none";
+  }else if(output=100001 && output>200000){
+    output.innerHTML = this.value.display = "none";
+  }else if(output=200001 && output>300000){
+    output.innerHTML = this.value.display = "none";
+ }else if(output=300001 && output>400000){
+    output.innerHTML = this.value.display = "none";
+}else if(output=400001 && output>500000){
+    output.innerHTML = this.value.display = "none";
+}else{
+    output.innerHTML = this.value;
+}*/
+
+}
